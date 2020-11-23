659c9deb4a5303110c9e795eac83b9f429a64add
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-11-23 09:39:59 +0200

    1.0.1

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 let X = null;
 let block = document.querySelector('.navbar');
+let Z;
 
 window.addEventListener("mousedown", element =>{
     X = element.clientX;
-    console.log("do");
 })
 
 window.addEventListener("mouseup", () => {
     X = null;
-    console.log("end");
+    if(Z > 150){
+        block.style.left = 0 + "%";
+    }if(Z < 150){
+        block.style.left = -100 + "%";
+    }
+    Z = null;
+    console.log(Z);
 })
 
 window.addEventListener("mousemove", element =>{
     if(X != null && X < element.clientX){
         
-        block.style.left = 0 + "%";
-
-        console.log(element.clientX)
+        Z = element.clientX - X; 
+        if(Z < 100 && Z > 0){
+            block.style.left = -100 + Z + "%";
+        }
     }else if(X != null && X > element.clientX){
-        block.style.left = -100 + "%";
+        block.style.left = -100 - Z + "%";
     }
+
+    console.log(Z);
 })
 
 
