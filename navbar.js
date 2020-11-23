056cd6e5e13fdbaec230321d120d2047ca9f98fd
Zvat Scorobogatov <zvat.333333@gmail.com>
2020-11-23 15:22:33 +0200

    2.0.0

---
+++ navbar.js
@@ -0,0 +1,53 @@
+let target = document.querySelector('.right-side');
+let block = document.querySelector('.slide');
+let XPoint = null;
+let Move = null;
+
+EventLoop = {
+    EventDown: function(event)
+    {
+        XPoint = event.clientX;
+
+
+        target.addEventListener("mousemove", EventLoop.EventMove);
+    },
+    EventMove: function(event)
+    {
+        Move = event.clientX - XPoint;
+        if(Move >= 200)
+        {
+            block.style.left = 0 + "px";
+        }else if(Move > 0 && Move > -200){
+            block.style.left = -200 + Move + "px";
+        }else if(Move < -200){
+            block.style.left = -200 + "px";
+        }
+
+
+        target.addEventListener("mouseup", EventLoop.EventUp);
+    }, 
+    EventUp: function(event)
+    {
+        if(Move >= 50){
+            block.style.left = 0 + "px";
+        }else{
+            block.style.left = -200 + "px";
+        }
+
+        XPoint = null;
+        Move = null;
+
+        target.addEventListener("mousedown", EventLoop.EventDown);
+        target.removeEventListener("mousemove", EventLoop.EventMove);
+ 
+    },
+    EventClick: function(event)
+    {
+        target.addEventListener("mousedown", EventLoop.EventDown);
+        target.addEventListener("mouseup", EventLoop.EventUp);
+    }
+}
+
+target.addEventListener("click", EventLoop.EventClick());
+
+
